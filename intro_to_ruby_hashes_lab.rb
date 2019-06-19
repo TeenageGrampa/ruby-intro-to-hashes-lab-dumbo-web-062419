60ae8ffb8a212e50cfae538c4bd1aa7693121667
Daniel Voigt <danielhvoigt@gmail.com>
2019-06-19 19:38:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,7 @@ end
 def monopoly_with_second_tier
 	railroads: {
 	  pieces: 4, 
-	  names: {}, 
+	  names: {reading_railroad:{}, pennsylvania_railroad: {}, b_and_o_railroad:{}, shortline:{}}, 
 	  rent_in_dollars: {}
 	}
 end
